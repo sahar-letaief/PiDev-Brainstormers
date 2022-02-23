c75e8df518509a18deefa23d689bc63bee8976d5
Sami Ben Chaabane <sami.ben.chaabane10@gmail.com>
2022-02-24 00:09:15 +0100

    Commentaire Entity

---
+++ src/Entity/Commentaire.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CommentaireRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CommentaireRepository::class)
+ */
+class Commentaire
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $userid;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $productid;
+
+    /**
+     * @ORM\Column(type="string", length=250)
+     */
+    private $comment;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $stars;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUserid(): ?int
+    {
+        return $this->userid;
+    }
+
+    public function setUserid(int $userid): self
+    {
+        $this->userid = $userid;
+
+        return $this;
+    }
+
+    public function getProductid(): ?int
+    {
+        return $this->productid;
+    }
+
+    public function setProductid(int $productid): self
+    {
+        $this->productid = $productid;
+
+        return $this;
+    }
+
+    public function getComment(): ?string
+    {
+        return $this->comment;
+    }
+
+    public function setComment(string $comment): self
+    {
+        $this->comment = $comment;
+
+        return $this;
+    }
+
+    public function getStars(): ?int
+    {
+        return $this->stars;
+    }
+
+    public function setStars(int $stars): self
+    {
+        $this->stars = $stars;
+
+        return $this;
+    }
+}
