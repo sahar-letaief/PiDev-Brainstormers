c506f2310eb53ea0454697ea75724d712ddbdc46
Sami Ben Chaabane <sami.ben.chaabane10@gmail.com>
2022-02-24 02:41:29 +0100

    Back Template

--- src/Form/CommentaireType.php
+++ src/Form/CommentaireType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Commentaire;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,10 +14,18 @@ class CommentaireType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('userid')
-            ->add('productid')
-            ->add('comment')
-            ->add('stars')
+            //->add('userid')
+            //->add('productid')
+        ->add('Comment', TextareaType::class)
+        ->add('stars', ChoiceType::class, [
+            'choices' =>[
+                '1'=> 1,
+                '2'=> 2,
+                '3'=> 3,
+                '4'=> 4,
+                '5'=> 5,
+            ],
+        ])
         ;
     }
 
