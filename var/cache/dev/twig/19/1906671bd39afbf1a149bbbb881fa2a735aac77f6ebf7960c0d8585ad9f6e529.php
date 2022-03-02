b113f3764c446f3e5ef229e9214e043105615237
yassinebensalha <yassine.bensalha@esprit.tn>
2022-03-02 22:44:23 +0100

    forgot about var folder

---
+++ var/cache/dev/twig/19/1906671bd39afbf1a149bbbb881fa2a735aac77f6ebf7960c0d8585ad9f6e529.php
@@ -0,0 +1,268 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* security/ActivateAccountWithCode.html.twig */
+class __TwigTemplate_242e8113fc11c508fd0d63e88494b24db5e62cac5fb53198700d7a285844a67a extends Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "template", "security/ActivateAccountWithCode.html.twig"));
+
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "template", "security/ActivateAccountWithCode.html.twig"));
+
+        // line 1
+        echo "
+";
+        // line 19
+        echo "<!DOCTYPE html>
+<html lang=\"en\">
+<head>
+    <!-- Required meta tags -->
+    <meta charset=\"utf-8\">
+    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\">
+    <title>Corona Admin</title>
+    <!-- plugins:css -->
+    <link rel=\"stylesheet\" href=\"";
+        // line 27
+        echo twig_escape_filter($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\AssetExtension']->getAssetUrl("Back/assets/vendors/mdi/css/materialdesignicons.min.css"), "html", null, true);
+        echo "\">
+    <link rel=\"stylesheet\" href=\"";
+        // line 28
+        echo twig_escape_filter($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\AssetExtension']->getAssetUrl("Back/assets/vendors/css/vendor.bundle.base.css"), "html", null, true);
+        echo "\">
+    <!-- endinject -->
+    <!-- Plugin css for this page -->
+    <!-- End plugin css for this page -->
+    <!-- inject:css -->
+    <!-- endinject -->
+    <!-- Layout styles -->
+    <link rel=\"stylesheet\" href=\"";
+        // line 35
+        echo twig_escape_filter($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\AssetExtension']->getAssetUrl("Back/assets/css/style.css"), "html", null, true);
+        echo "\">
+    <!-- End layout styles -->
+    <link rel=\"shortcut icon\" href=\"";
+        // line 37
+        echo twig_escape_filter($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\AssetExtension']->getAssetUrl("Back/assets/images/favicon.png"), "html", null, true);
+        echo "\" />
+</head>
+<body>
+<div class=\"container-scroller\">
+    <div class=\"container-fluid page-body-wrapper full-page-wrapper\">
+        <div class=\"row w-100 m-0\">
+            <div class=\"content-wrapper full-page-wrapper d-flex align-items-center auth login-bg\">
+                <div class=\"card col-lg-4 mx-auto\">
+                    <div class=\"card-body px-5 py-5\">
+                        <h3 class=\"card-title text-center mb-3\">Insert Code</h3>
+
+
+                        <form method=\"post\">
+                            ";
+        // line 50
+        if ((isset($context["error"]) || array_key_exists("error", $context) ? $context["error"] : (function () { throw new RuntimeError('Variable "error" does not exist.', 50, $this->source); })())) {
+            // line 51
+            echo "                                <ul style=\"background-color: red;width: 100%;text-align: left;border-radius: 5px;font-size: 17px;\" >
+                                    <li style=\"padding-left: 20px\">";
+            // line 52
+            echo twig_escape_filter($this->env, (isset($context["error"]) || array_key_exists("error", $context) ? $context["error"] : (function () { throw new RuntimeError('Variable "error" does not exist.', 52, $this->source); })()), "html", null, true);
+            echo "</li>
+                                </ul>
+                            ";
+        }
+        // line 55
+        echo "                            <div class=\"form-group\">
+                                <label>Verification Code</label>
+                                <input placeholder=\"Code\" name=\"verificationcode\" class=\"form-control p_input\" required autofocus>
+                            </div>
+
+                            <div class=\"text-center\">
+                                <button type=\"submit\" class=\"btn btn-primary btn-block enter-btn\">Submit</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <!-- content-wrapper ends -->
+        </div>
+        <!-- row ends -->
+    </div>
+    <!-- page-body-wrapper ends -->
+</div>
+<!-- container-scroller -->
+<!-- plugins:js -->
+<script src=\"";
+        // line 75
+        echo twig_escape_filter($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\AssetExtension']->getAssetUrl("Back/assets/vendors/js/vendor.bundle.base.js"), "html", null, true);
+        echo "\"></script>
+<!-- endinject -->
+<!-- Plugin js for this page -->
+<!-- End plugin js for this page -->
+<!-- inject:js -->
+<script src=\"";
+        // line 80
+        echo twig_escape_filter($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\AssetExtension']->getAssetUrl("Back/assets/js/off-canvas.js"), "html", null, true);
+        echo "\"></script>
+<script src=\"";
+        // line 81
+        echo twig_escape_filter($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\AssetExtension']->getAssetUrl("Back/assets/js/hoverable-collapse.js"), "html", null, true);
+        echo "\"></script>
+<script src=\"";
+        // line 82
+        echo twig_escape_filter($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\AssetExtension']->getAssetUrl("Back/assets/js/misc.js"), "html", null, true);
+        echo "\"></script>
+<script src=\"";
+        // line 83
+        echo twig_escape_filter($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\AssetExtension']->getAssetUrl("Back/assets/js/settings.js"), "html", null, true);
+        echo "\"></script>
+<script src=\"";
+        // line 84
+        echo twig_escape_filter($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\AssetExtension']->getAssetUrl("Back/assets/js/todolist.js"), "html", null, true);
+        echo "\"></script>
+<!-- endinject -->
+</body>
+</html>
+";
+        
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->leave($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof);
+
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+    }
+
+    public function getTemplateName()
+    {
+        return "security/ActivateAccountWithCode.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  148 => 84,  144 => 83,  140 => 82,  136 => 81,  132 => 80,  124 => 75,  102 => 55,  96 => 52,  93 => 51,  91 => 50,  75 => 37,  70 => 35,  60 => 28,  56 => 27,  46 => 19,  43 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("
+{#
+
+{% if app.user %}
+<div class=\"mb-3\">
+   You are logged in as {{ app.user.username }}, <a href=\"{{ path('app_logout') }}\">Logout</a>
+</div>
+{% endif %}
+
+Uncomment this section and add a remember_me option below your firewall to activate remember me functionality.
+See https://symfony.com/doc/current/security/remember_me.html
+
+<div class=\"checkbox mb-3\">
+   <label>
+       <input type=\"checkbox\" name=\"_remember_me\"> Remember me
+   </label>
+</div>
+#}
+<!DOCTYPE html>
+<html lang=\"en\">
+<head>
+    <!-- Required meta tags -->
+    <meta charset=\"utf-8\">
+    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\">
+    <title>Corona Admin</title>
+    <!-- plugins:css -->
+    <link rel=\"stylesheet\" href=\"{{ asset('Back/assets/vendors/mdi/css/materialdesignicons.min.css') }}\">
+    <link rel=\"stylesheet\" href=\"{{ asset('Back/assets/vendors/css/vendor.bundle.base.css') }}\">
+    <!-- endinject -->
+    <!-- Plugin css for this page -->
+    <!-- End plugin css for this page -->
+    <!-- inject:css -->
+    <!-- endinject -->
+    <!-- Layout styles -->
+    <link rel=\"stylesheet\" href=\"{{ asset('Back/assets/css/style.css') }}\">
+    <!-- End layout styles -->
+    <link rel=\"shortcut icon\" href=\"{{ asset('Back/assets/images/favicon.png') }}\" />
+</head>
+<body>
+<div class=\"container-scroller\">
+    <div class=\"container-fluid page-body-wrapper full-page-wrapper\">
+        <div class=\"row w-100 m-0\">
+            <div class=\"content-wrapper full-page-wrapper d-flex align-items-center auth login-bg\">
+                <div class=\"card col-lg-4 mx-auto\">
+                    <div class=\"card-body px-5 py-5\">
+                        <h3 class=\"card-title text-center mb-3\">Insert Code</h3>
+
+
+                        <form method=\"post\">
+                            {% if (error) %}
+                                <ul style=\"background-color: red;width: 100%;text-align: left;border-radius: 5px;font-size: 17px;\" >
+                                    <li style=\"padding-left: 20px\">{{ error }}</li>
+                                </ul>
+                            {% endif %}
+                            <div class=\"form-group\">
+                                <label>Verification Code</label>
+                                <input placeholder=\"Code\" name=\"verificationcode\" class=\"form-control p_input\" required autofocus>
+                            </div>
+
+                            <div class=\"text-center\">
+                                <button type=\"submit\" class=\"btn btn-primary btn-block enter-btn\">Submit</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <!-- content-wrapper ends -->
+        </div>
+        <!-- row ends -->
+    </div>
+    <!-- page-body-wrapper ends -->
+</div>
+<!-- container-scroller -->
+<!-- plugins:js -->
+<script src=\"{{ asset('Back/assets/vendors/js/vendor.bundle.base.js') }}\"></script>
+<!-- endinject -->
+<!-- Plugin js for this page -->
+<!-- End plugin js for this page -->
+<!-- inject:js -->
+<script src=\"{{ asset('Back/assets/js/off-canvas.js') }}\"></script>
+<script src=\"{{ asset('Back/assets/js/hoverable-collapse.js') }}\"></script>
+<script src=\"{{ asset('Back/assets/js/misc.js') }}\"></script>
+<script src=\"{{ asset('Back/assets/js/settings.js') }}\"></script>
+<script src=\"{{ asset('Back/assets/js/todolist.js') }}\"></script>
+<!-- endinject -->
+</body>
+</html>
+", "security/ActivateAccountWithCode.html.twig", "C:\\Users\\Yassine\\Desktop\\git-login-symfony\\templates\\security\\ActivateAccountWithCode.html.twig");
+    }
+}
